96222c220707f503526cd14242081b28ea9dad98
n0psledbyte <n0psledbyte@gmail.com>
2018-06-14 17:03:56 +0700

    initial work untuk halaman ujian siswa

---
+++ src/components/mulaiUjian.js
@@ -0,0 +1,117 @@
+import React from 'react';
+import {Pagination, PageLink, PageItem} from 'mdbreact';
+import {Input, Form} from 'mdbreact'
+import {Fa, Button, Container, Row, Col} from 'mdbreact';
+
+function InputRadio(props) {
+    return (
+        <div className="jawaban">
+            <input type="radio"  {...props} />
+            <label htmlFor={props.id}>
+                <span></span>
+                {props.label}
+            </label>
+        </div>
+    )
+}
+function inRange(min, max, value) {
+    if(value < min) {
+        return min;
+    } else if (value > max) {
+        return max;
+    } else {
+        return value;
+    }
+}
+class MulaiUjian extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            'pertanyaan': "Berpakah nilai 1 + 1 sama dengan sama dengan sama dengan sama dengan sama dengan sama dengan aaaa bbbb bbbbbb baafd fdsafsadf sdf fsdaf sdaf sadf sadf sdaf asdf sadf sadf asd? fsdf sadf asdf asdf sadf sadf asdf asd  fsdf asdf sadf asdf asd",
+            current_soal: 1,
+            max_soal: 40,
+            answered: [],
+        }
+    }
+    setCurrentSoal(i) {
+        this.setState({current_soal: i});
+    }
+    setCurrentSoalRelatif(relatif) {
+        let current_soal = this.state.current_soal + relatif;
+        current_soal = inRange(1, this.state.max_soal, current_soal)
+        this.setCurrentSoal(current_soal);
+    }
+    renderItem(len, active=1, answered=[]) {
+        var items = [];
+        var range_list = [...Array(len).keys()].map(i => i+1);
+        range_list.forEach((i) => {
+            let apn = "";
+            let is_active = false;
+            if(i == active) {
+                is_active = true;
+            }
+            if(!is_active && answered.includes(i)) {
+                apn = "green text-white z-depth-1";
+            }
+            items.push(
+                <PageItem key={i} active={is_active} onClick={() => this.setCurrentSoal(i)}>
+                    <PageLink className={"page-link "+apn}>
+                        {i}
+                    </PageLink>
+                </PageItem>
+            );
+        });
+        return items;
+    }
+    renderPagination() {
+        return (
+            <Pagination className="pg-blue flex-wrap text-white">
+                {this.renderItem(40, this.state.current_soal, [1, 2, 9])}
+            </Pagination>
+        )
+    }
+    render() {
+        return (
+            <Container fluid={true} className="ujian-body">
+                <Container className="pt-3">
+                    <Row className="">
+                        <Col md="10" className="mx-auto px-0 py-0">
+                            {this.renderPagination()}
+                        </Col>
+                        <Col md="10" className="mx-auto border status rounded z-depth-2">
+                            <span>Ulangan Harian Bahasa Indoensia</span>
+                            <span className="float-right">02.00</span>
+                        </Col>
+                        <Col md="10" className="pt-5 mx-auto soal rounded grey lighten-3 text-dark text-left z-depth-2">
+                            <p className="pertanyaan text-center" style={{fontSize: (30 - this.state.pertanyaan.length/15)} }>{this.state.pertanyaan}</p>
+                            <form className="answer pl-5 pt-4">
+                                <Col sm="12" className="mx-auto answer pl-5">
+                                    <Row className="pl-5">
+                                        <Col sm="6" className="mb-3">
+                                            <InputRadio id="radio01" name="radio" label="Use our position utilities to place navbars in non-static positions. Choose from fixed to the top, fixed to the bottom,"/>
+                                        </Col>
+                                        <Col sm="6">
+                                            <InputRadio id="radio02" name="radio" label="Jawabannya 20"/>
+                                        </Col>
+                                        <Col sm="6">
+                                            <InputRadio id="radio03" name="radio" label="Jawabannya 30"/>
+                                        </Col>
+                                        <Col sm="6">
+                                            <InputRadio id="radio04" name="radio" label="Jawabannya 40"/>
+                                        </Col>
+                                    </Row>
+                                </Col>
+                            </form>
+                        </Col>
+                        <div className="direction mx-auto my-3 text-center arrow">
+                            <Button size="lg" color="info" onClick={() => this.setCurrentSoalRelatif(-1)}><Fa icon="arrow-circle-left"/> Prev</Button>
+                            <Button size="lg" color="info" onClick={() => this.setCurrentSoalRelatif(1)}>Next <Fa icon="arrow-circle-right"/></Button>
+                        </div>
+                    </Row>
+                </Container>
+            </Container>
+        );
+    }
+}
+
+export default MulaiUjian;
--- src/components/siswa.js
+++ src/components/siswa.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import {Button} from 'reactstrap';
+import {Button, Container, Row, Col} from 'mdbreact';
+import {Navbar, NavbarBrand, NavbarNav, NavbarText, NavItem, NavLink, Fa} from 'mdbreact';
+import {Modal, ModalBody, ModalHeader, ModalFooter} from 'mdbreact'
 import {store} from '../stores/store';
 import {unsetProfile} from '../actions/profile';
 import {unsetAuth} from '../actions/auth';
+import {BrowserRouter as Router, Switch, Route, HashRouter, Link} from 'react-router-dom';
+import MulaiUjian from './mulaiUjian';
 
 class SiswaDashboard extends React.Component {
     constructor(props) {
@@ -24,12 +28,78 @@ class SiswaDashboard extends React.Component {
     }
     render() {
         return (
-        <div>
-            <h2>Ini halaman siswa</h2><br/>
-            <Button onClick={this.logout.bind(this)}>Logout</Button>
-        </div>
+            <HashRouter basename="/siswa">
+                <Container fluid={true} className="px-0">
+                    <Navbar color="indigo" dark expand="md">
+                        <NavbarBrand href="/">
+                            <strong>SmartExam</strong>
+                        </NavbarBrand>
+                        <NavbarNav left>
+                            <NavItem active>
+                                <NavLink to="/">Home</NavLink>
+                            </NavItem>
+                            <NavItem>
+                                <NavLink to="#">Settings</NavLink>
+                            </NavItem>
+                        </NavbarNav>
+                        <NavbarNav right>
+                            <NavItem>
+                                <Button size="sm" color="indigo" onClick={() => this.logout()}><Fa icon="star" /> Logout</Button>
+                            </NavItem>
+                        </NavbarNav>
+                    </Navbar>
+                    <Switch>
+                        <Route exact path="/" component={DaftarUjian} />)} />
+                        <Route path="/ujian" component={MulaiUjian} />
+                    </Switch>
+                </Container>
+            </HashRouter>
         );
     }
 }
 
+class DaftarUjian extends React.Component {
+    constructor(props) {
+        super(props);
+        this.toggle = this.toggle.bind(this);
+        this.state = {
+            modal: false,
+        };
+    }
+    toggle() {
+        this.setState({
+            modal: !this.state.modal
+        })
+    }
+    render() {
+        return (
+        <HashRouter basename="/siswa">
+            <Container className="text-center pt-5">
+                <h2 class="title">Ujian yang tersedia</h2>
+                <Row className="mt-5">
+                    <Col md="4" sm="6" onClick={this.toggle} className="text-center white-text z-depth-2 box-border bg-primary">
+                        <h5>Ulangan Tengah Semester Bahasa Indonesia</h5>
+                    </Col>
+                    <Col md="4" sm="6" className="box-border white-text default-color-dark z-depth-2">
+                        <h5>Ulangan Harian Bahasa Indonesia</h5>
+                    </Col>
+                    <Col md="4" sm="6" className="box-border white-text info-color-dark z-depth-2">
+                        <h5>Ulangan Harian Bahasa Indonesia</h5>
+                    </Col>
+                </Row>
+                <Modal isOpen={this.state.modal} toggle={this.toggle}>
+                <ModalHeader toggle={this.toggle}>Ulangan Bahasa Indonesia</ModalHeader>
+                <ModalBody>
+                    Apakah anda yakin untuk memulai ujian ini?
+                </ModalBody>
+                <ModalFooter>
+                    <Button color="danger" onClick={this.toggle}>Cancel</Button>{' '}
+                    <Link to="/ujian"><Button color="success">Accept</Button></Link>
+                </ModalFooter>
+                </Modal>
+            </Container>
+        </HashRouter>
+        );
+    }
+}
 export default SiswaDashboard;
