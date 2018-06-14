c94d7a0c54087f74887ffeccddd52651825a1ef3
n0psledbyte <n0psledbyte@gmail.com>
2018-06-14 17:06:17 +0700

    update login.js

--- src/components/login.js
+++ src/components/login.js
@@ -4,12 +4,21 @@ import {checkLogin} from '../util/api';
 import {setAuth} from '../actions/auth';
 import {setProfile} from '../actions/profile';
 import {store} from '../stores/store';
+import '../css/main.css';
+import '../css/colors.css';
+import loginBg from '../img/loginBg.jpg';
+import 'font-awesome/css/font-awesome.min.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+import 'mdbootstrap/css/bootstrap.min.css';
+import 'mdbootstrap/css/mdb.min.css';
 
 function InputForm(props) {
     return (
         <div className="input-group shadow-sm">
             <div className="input-group-prepend">
-                <span className="input-group-text"><i className={"fa " + props.icon}></i></span>
+                <span className="input-group-text">
+                    <i className={"fa " + props.icon}></i>
+                </span>
             </div>
             <Input type="text" className="form-control" {...props}/>
         </div>
@@ -67,9 +76,10 @@ class Login extends React.Component {
         <Container fluid={true} className="body">
             <Container>
                 <Row>
-                    <Col sm={{size: 4, offset: 4}} className="login center-screen text-center text-white rounded shadow bg-success">
+                    <Col sm={{size: 4, offset: 4}}
+                        className="login center-screen text-center text-white rounded shadow bg-info">
                         <h3>Login Form</h3>
-                        <Col sm="12" className="form-login bg-white text-dark">
+                        <Col sm="12" className="form-login bg-white text-dark rounded">
                             <div className="status bg-danger text-white rounded">{errormsg}</div>
                             <form>
                                 <div className="input-userpass">
