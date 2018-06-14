7b5a2138a7beb066417aa840444a993179ba2545
n0psledbyte <n0psledbyte@gmail.com>
2018-06-14 17:02:42 +0700

    setting ke halaman login baru

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import React, { Component } from 'react';
-import Login from './components/login';
+import Login from './components/newlogin';
 import SiswaDashboard from './components/siswa';
 import GuruDashboard from './components/guru';
 import store from './stores/store';
 import {BrowserRouter as Router, Switch, Route, HashRouter} from 'react-router-dom';
-import './css/main.css';
-import 'font-awesome/css/font-awesome.min.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 class App extends Component {
-  render() {
-    return (
-        <HashRouter basename="/">
+    render() {
+        return (
+            <HashRouter basename="/">
                 <Switch>
                     <Route exact path="/" component={Login} />)} />
                     <Route path="/guru" component={GuruDashboard} />
                     <Route path="/siswa" component={SiswaDashboard} />
                 </Switch>
-        </HashRouter>
-    );
-  }
+            </HashRouter>
+        );
+    }
 }
 
 export default App;
