5e0dce6bc9b4636d806ca89b83e22e5ce34e99f0
n0psledbyte <n0psledbyte@gmail.com>
2018-06-16 21:06:39 +0700

    change login form color

--- src/components/newlogin.js
+++ src/components/newlogin.js
@@ -58,7 +58,7 @@ class Login extends React.Component {
         <Container fluid={true} className="body">
             <Container>
                 <Row>
-                    <Col sm="4" className="login center-screen text-center text-white rounded shadow bg-info mx-auto">
+                    <Col sm="4" className="login primary-color center-screen text-center text-white rounded shadow mx-auto">
                         <h3>Login Form</h3>
                         <Col sm="12" className="form-login bg-white text-dark text-left rounded">
                             <div className="status bg-danger text-white rounded">{}</div>
