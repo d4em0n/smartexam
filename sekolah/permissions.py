6ee511d44dac08d6ba8d56a3973ad53e9d4c7402
n0psledbyte <n0psledbyte@gmail.com>
2018-06-18 14:20:31 +0700

    add permission

--- sekolah/permissions.py
+++ sekolah/permissions.py
@@ -8,3 +8,11 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return obj.user == request.user or request.user.is_staff
+
+class IsPembuatUjianOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        pk = self.request.pk
+        print(pk)
+        if request.method in permissions.SAFE_METHOD:
+            return True
+        return request.user.is_guru and request.user.guru.ujian_set.filter(id_ujian=pk).exists()
